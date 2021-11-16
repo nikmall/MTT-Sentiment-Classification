b33fba199dc72a05744b7142cc650340cd17d2ea
nikmall <65458222+nikmall@users.noreply.github.com>
2021-11-16 10:19:05 +0200

    Tuning

--- parameters.py
+++ parameters.py
@@ -26,8 +26,8 @@ param_mtt = {
     "hid_dim": 300,# embedding dimension if glove300 is always 300
     "enc_layers": 3,
     "dec_layers": 3,
-    "enc_heads": 6,
-    "dec_heads": 6,
+    "enc_heads": 5,
+    "dec_heads": 5,
     "enc_pf_dim": 600,
     "dec_pf_dim": 600,
     "enc_dropout": 0.3,
