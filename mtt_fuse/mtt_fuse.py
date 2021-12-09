fdd2f4bb62d8cbcbf5279de9c5e74ffe33a52347
nikmall <65458222+nikmall@users.noreply.github.com>
2021-12-09 20:43:32 +0200

    Fixed weight of dec loss

--- mtt_fuse/mtt_fuse.py
+++ mtt_fuse/mtt_fuse.py
@@ -131,7 +131,7 @@ def train(model, train_loader, optimizer, criterion, params, device, clip=10):
 
         decoded, regression_score = model(src, trg, label)
 
-        translate_loss = params['loss_dec_weight'] * 1.5 * criterion(decoded, trg)
+        translate_loss = params['loss_dec_weight'] * criterion(decoded, trg)
         #if params["cyclic"]:
         #    translate_cycle_loss = params['loss_dec_cycle_weight'] * criterion(cycled_decoded, src)
         translate_sent_loss = params['loss_regress_weight'] * criterion(regression_score, label)
