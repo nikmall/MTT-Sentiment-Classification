1d93e63a1219ec5c3f39e4778673d003f6e558a6
nikmall <65458222+nikmall@users.noreply.github.com>
2021-12-19 20:20:35 +0200

    Added standard scaling

--- data_process.py
+++ data_process.py
@@ -4,6 +4,8 @@ from dataset import Multimodal_Datasets
 from torch.utils.data import DataLoader
 from parameters import param
 
+from sklearn.preprocessing import StandardScaler
+
 
 def get_data(dataset, split='train'):
     alignment = 'a' if param["aligned"] else 'na'
@@ -19,10 +21,13 @@ def get_data(dataset, split='train'):
     return data
 
 
-def get_dataloaders(dataset):
+def get_dataloaders(dataset, scale=True):
     train_data = get_data(dataset, 'train')
     valid_data = get_data(dataset, 'valid')
     test_data = get_data(dataset, 'test')
+
+    if scale:
+        scale_data(train_data, valid_data, test_data)
 
     train_loader = DataLoader(train_data, batch_size=param["batch_size"], shuffle=True)
     valid_loader = DataLoader(valid_data, batch_size=param["batch_size"], shuffle=True)
@@ -30,3 +35,69 @@ def get_dataloaders(dataset):
 
     return train_loader, valid_loader, test_loader
 
+def scale_data(train_data, valid_data, test_data):
+    # audio
+    train_audio = train_data.audio.detach().numpy()
+    audio_shape = train_audio.shape
+    train_audio = train_audio.reshape(audio_shape[0] * audio_shape[1], audio_shape[2])
+    audio_scaler = StandardScaler()
+    audio_scaler.fit(train_audio)
+    train_audio = audio_scaler.transform(train_audio)
+    train_data.audio = torch.tensor(train_audio.reshape(audio_shape[0], audio_shape[1], audio_shape[2])).cpu().detach()
+
+    valid_audio = valid_data.audio.detach().numpy()
+    valid_audio = valid_audio.reshape(-1, audio_shape[2])
+    valid_audio = audio_scaler.transform(valid_audio)
+    valid_data.audio = torch.tensor(valid_audio.reshape(-1, audio_shape[1], audio_shape[2])).cpu().detach()
+
+    test_audio = test_data.audio.detach().numpy()
+    test_audio = test_audio.reshape(-1, audio_shape[2])
+    test_audio = audio_scaler.transform(test_audio)
+    test_data.audio = torch.tensor(test_audio.reshape(-1, audio_shape[1], audio_shape[2])).cpu().detach()
+
+    # Text
+    train_text = train_data.text.detach().numpy()
+    text_shape = train_text.shape
+    train_text = train_text.reshape(text_shape[0] * text_shape[1], text_shape[2])
+    text_scaler = StandardScaler()
+    text_scaler.fit(train_text)
+    train_text = text_scaler.transform(train_text)
+    train_data.text = torch.tensor(train_text.reshape(-1, text_shape[1], text_shape[2])).cpu().detach()
+
+    valid_text = valid_data.text.detach().numpy()
+    valid_text = valid_text.reshape(-1, text_shape[2])
+    valid_text = text_scaler.transform(valid_text)
+    valid_data.text = torch.tensor(valid_text.reshape(-1, text_shape[1], text_shape[2])).cpu().detach()
+
+    test_text = test_data.text.detach().numpy()
+    test_text = test_text.reshape(-1, text_shape[2])
+    test_text = text_scaler.transform(test_text)
+    test_data.text = torch.tensor(test_text.reshape(-1, text_shape[1], text_shape[2])).cpu().detach()
+
+    # Vision
+    train_vision = train_data.vision.detach().numpy()
+    vision_shape = train_vision.shape
+    train_vision = train_vision.reshape(vision_shape[0] * vision_shape[1], vision_shape[2])
+    vision_scaler = StandardScaler()
+    train_vision = vision_scaler.fit_transform(train_vision)
+    train_data.vision = torch.tensor(train_vision.reshape(-1, vision_shape[1], vision_shape[2])).cpu().detach()
+
+    valid_vision = valid_data.vision.detach().numpy()
+    valid_vision = valid_vision.reshape(-1, vision_shape[2])
+    valid_vision = vision_scaler.transform(valid_vision)
+    valid_data.vision = torch.tensor(valid_vision.reshape(-1, vision_shape[1], vision_shape[2])).cpu().detach()
+
+    test_vision = test_data.vision.detach().numpy()
+    test_vision = test_vision.reshape(-1, vision_shape[2])
+    test_vision = vision_scaler.transform(test_vision)
+    test_data.vision = torch.tensor(test_vision.reshape(-1, vision_shape[1], vision_shape[2])).cpu().detach()
+
+    # return
+
+def scale_2d_standard(x, mu=None, std=None):
+    if mu is None and std is None:
+        mu = x.mean(axis=(0, 1), keepdims=True)
+        std = x.std(axis=(0, 1), keepdims=True)
+    x = (x - mu) / (std + 1e-10)
+
+    return x, mu, std
--- parameters.py
+++ parameters.py
@@ -26,10 +26,10 @@ param_mtt = {
     "hid_dim": 300,# embedding dimension if glove300 is always 300
     "enc_layers": 3,
     "dec_layers": 3,
-    "enc_heads": 6,
-    "dec_heads": 6,
-    "enc_pf_dim": 480,
-    "dec_pf_dim": 440,
+    "enc_heads": 5,
+    "dec_heads": 5,
+    "enc_pf_dim": 520,
+    "dec_pf_dim": 480,
     "enc_dropout": 0.31,
     "dec_dropout": 0.31,
 
