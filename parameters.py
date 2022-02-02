ccf98eb904bc26fc3a134dd030d1df5f6bd50151
nikmall <65458222+nikmall@users.noreply.github.com>
2022-02-02 21:14:38 +0200

    Test seed 202. Cyclic to .11

--- parameters.py
+++ parameters.py
@@ -83,7 +83,7 @@ param_mtt_fuse = {
     "n_epochs": 150,
     'lr_patience': 20,
     'loss_dec_weight': 0.15,
-    'loss_dec_cycle_weight': 0.1,
+    'loss_dec_cycle_weight': 0.11,
     'loss_regress_weight': 0.9,
 
     'fuse_modalities': True,
