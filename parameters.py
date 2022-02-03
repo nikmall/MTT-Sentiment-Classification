39e2fbe4bc78f223923fca998f910735a2cdc05d
nikmall <65458222+nikmall@users.noreply.github.com>
2022-02-03 21:03:01 +0200

    Same with .1 losses

--- parameters.py
+++ parameters.py
@@ -82,8 +82,8 @@ param_mtt_fuse = {
 
     "n_epochs": 150,
     'lr_patience': 20,
-    'loss_dec_weight': 0.15,
-    'loss_dec_cycle_weight': 0.11,
+    'loss_dec_weight': 0.1,
+    'loss_dec_cycle_weight': 0.1,
     'loss_regress_weight': 0.9,
 
     'fuse_modalities': True,
