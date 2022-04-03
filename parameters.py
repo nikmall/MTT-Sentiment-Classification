fe19f2334a377fa7d888b86dbf184ef6038ae505
nikmall <nikolaosmallios@outlook.com>
2022-04-03 17:52:44 +0300

    sent_dropout": 0.25

--- parameters.py
+++ parameters.py
@@ -69,9 +69,9 @@ param_mtt_fuse = {
     "dec_heads": 6,
     "enc_pf_dim": 490,
     "dec_pf_dim": 490,
-    "enc_dropout": 0.30,
-    "dec_dropout": 0.30,
-    'att_dropout': 0.25,
+    "enc_dropout": 0.31,
+    "dec_dropout": 0.31,
+    'att_dropout': 0.30,
 
     "sent_hid_dim": 156,
     "sent_final_hid": 132,
