2a2ff0094ed054b400f264f83e61bd59c8484c92
nikmall <nikolaosmallios@outlook.com>
2022-05-09 00:01:17 +0300

    Back to standard initialization. regress on output_2

--- tools.py
+++ tools.py
@@ -32,7 +32,7 @@ def init_weights(m, rand=True):
     if rand:
         for name, param in m.named_parameters():
             if 'weight' in name:
-                nn.init.normal_(param.data, mean=0, std=0.005)
+                nn.init.normal_(param.data, mean=0, std=0.01)
             else:
                 nn.init.constant_(param.data, 0)
     else:
